bf9a3c6a04baced3d90d241b36bb6408edc907e6
Shoaib <syedshoaib1369@gmail.com>
2024-02-11 22:22:35 +0100

    changes made

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel
-from lib.preprocessing import encode_cols, load_preprocessor
-from lib.models import get_model
+from preprocessing import encode_cols, load_preprocessor
+from models import get_model
 from sklearn.feature_extraction import DictVectorizer
 from typing import List
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator
-from web_service.config import PATH_TO_PREPROCESSOR, PATH_TO_MODEL, CATEGORICAL_COLS
+from config import PATH_TO_PREPROCESSOR, PATH_TO_MODEL, CATEGORICAL_COLS
  
  
 app = FastAPI()
@@ -32,7 +32,7 @@ def run_inference(user_input: List[InputData], dv: DictVectorizer, model: BaseEs
  
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to the Inference API!"}
+    return {"message": "Welcome to the Diamond Price Predictor!"}
  
 @app.post("/predict_diamond_price")
 def predict_duration_route(payload: InputData):
